3197f2a24cc13d2c4b7856e149e977e7283fca53
whirlxd <ninja21official@gmail.com>
2025-02-27 23:39:10 +0530

    update navbar and organizations responsive

--- app/mycalendars/page.tsx
+++ app/mycalendars/page.tsx
@@ -2,11 +2,12 @@
 
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
-import { MapPin, SlidersHorizontal } from "lucide-react";
+import { CalendarIcon, SlidersHorizontal } from "lucide-react";
 import { useEffect, useMemo, useState } from "react";
-import { SaveButton } from "@/components/ui/save-button";
+
 import type mongoose from "mongoose";
 import type { Document } from "mongoose";
+import { EventCard } from "@/components/event-card";
 import {
 	Select,
 	SelectContent,
@@ -16,7 +17,6 @@ import {
 	SelectTrigger,
 	SelectValue,
 } from "@/components/ui/select";
-import Link from "next/link";
 import SkeletonCard from "@/app/events/SkeletonCard";
 import { Input } from "@/components/ui/input";
 import debounce from "lodash/debounce";
@@ -59,6 +59,7 @@ export default function Home() {
 	const [selectedOrg, setSelectedOrg] = useState<string>("all");
 	const [loading, setLoading] = useState<boolean>(true);
 	const [searchTerm, setSearchTerm] = useState("");
+	const [mobileCalendarOpen, setMobileCalendarOpen] = useState(false);
 
 	// Debounced search function
 	const debouncedSearch = debounce(async (searchTerm: string) => {
@@ -161,22 +162,43 @@ export default function Home() {
 	}, []);
 
 	return (
-		<div className="mt-[3.75rem] flex flex-col gap-[2.25rem] w-[69.375rem]">
-			<div className="w-full flex flex-row justify-between items-center">
-				<h3 className="text-[2rem] font-jakarta font-extrabold">
-					My Saved Events
-				</h3>
+		<div className="mt-[3.75rem] flex flex-col gap-[2.25rem] w-[69.375rem] max-sm:w-full max-sm:px-5 max-sm:mt-5">
+			<div className="w-full flex flex-row justify-between items-center max-sm:flex-col max-sm:items-start">
+				<div className="flex flex-row items-center gap-2 max-sm:w-full">
+					<h3 className="text-[2rem] font-jakarta font-extrabold max-sm:text-[18px] ">
+						Upcoming Events
+					</h3>
+					<div className="flex gap-2.5 max-sm:ml-auto sm:hidden">
+						<Button
+							variant={date && date >= today ? "default" : "secondary"}
+							onClick={() => setDate(new Date())}
+							className="rounded-[0.75rem] h-11 font-medium w-auto py-2"
+						>
+							Upcoming
+						</Button>
+						<Button
+							variant={date && date < today ? "default" : "secondary"}
+							onClick={() => {
+								const pastDate = new Date();
+								pastDate.setMonth(pastDate.getMonth() - 1);
+								setDate(pastDate);
+							}}
+							className="rounded-[0.75rem] h-11 font-medium w-auto py-2"
+						>
+							Past
+						</Button>
+					</div>
+				</div>
 				<Input
 					type="search"
 					placeholder="Search..."
-					className="md:w-[10rem] rounded-[0.75rem] mr-3 bg-white h-[3.125rem]"
+					className="md:w-[10rem] rounded-[0.75rem] mr-3 bg-white h-[3.125rem] max-sm:w-full max-sm:mt-2"
 					onChange={(e) => handleSearch(e.target.value)}
 					value={searchTerm}
 				/>
-
-				<div className="flex flex-row items-center gap-[0.75rem]">
+				<div className="flex flex-row items-center gap-[0.75rem] max-sm:w-full max-sm:mt-2">
 					<Select>
-						<SelectTrigger className="w-[15.625rem] h-[3.125rem] bg-white rounded-[0.75rem]">
+						<SelectTrigger className="w-[15.625rem] h-[3.125rem] bg-white rounded-[0.75rem] max-sm:w-full">
 							<SelectValue placeholder="Anywhere" />
 						</SelectTrigger>
 						<SelectContent className="bg-white">
@@ -187,17 +209,16 @@ export default function Home() {
 							</SelectGroup>
 						</SelectContent>
 					</Select>
-					<Button className="bg-[#1C2329] rounded-[0.74969rem] h-[3.125rem] text-white px-[0.81rem]">
+					<Button className="bg-[#1C2329] rounded-[0.74969rem] h-[3.125rem] text-white px-[0.81rem] max-sm:w-full max-sm:h-11">
 						<SlidersHorizontal size={20} />
 						<span className="text-[1rem]">Filters (1)</span>
 					</Button>
 				</div>
-
 				<Select
 					value={selectedOrg}
 					onValueChange={(val) => setSelectedOrg(val)}
 				>
-					<SelectTrigger className="w-[15.625rem] h-[3.125rem] bg-white rounded-[0.75rem]">
+					<SelectTrigger className="w-[15.625rem] h-[3.125rem] bg-white rounded-[0.75rem] max-sm:mt-5 max-sm:w-full">
 						<SelectValue placeholder="Select Organization" />
 					</SelectTrigger>
 					<SelectContent className="bg-white">
@@ -213,29 +234,80 @@ export default function Home() {
 					</SelectContent>
 				</Select>
 			</div>
-			<div className="flex">
-				<ol className="relative flex-grow pr-20 border-s border-[#808080] border-opacity-25">
-					{loading
-						? Array.from({ length: 5 }).map((_, i) => <SkeletonCard key={i} />)
-						: Object.entries(groupedEvents).map(([date, groupedEvents]) => (
-								<li key={date} className="mb-10 ms-4">
-									<div className="absolute w-3 h-3 bg-gray-800 rounded-full mt-1.5 -start-1.5 border border-white"></div>
-									<time className="mb-1 text-xl font-semibold leading-none">
-										{date}
-									</time>
-									{groupedEvents.map((event) => (
-										<EventCard event={event} key={event._id} />
-									))}
-								</li>
-							))}
+
+			<div className="flex max-sm:flex-col">
+				<ol className="relative flex-grow pr-20 border-s border-[#808080] max-sm:border-none border-opacity-25 max-sm:pr-0 max-sm:px-4">
+					<>
+						<div className="absolute right-4 top-2 sm:hidden">
+							<Button
+								variant="outline"
+								onClick={() => setMobileCalendarOpen(true)}
+								className="p-2"
+							>
+								<CalendarIcon size={20} />
+							</Button>
+						</div>
+
+						{mobileCalendarOpen && (
+							<div className="fixed inset-0 flex items-center justify-center bg-black backdrop-blur-sm bg-opacity-50 z-50 sm:hidden">
+								<div className="bg-white p-4 rounded-md">
+									<Calendar
+										mode="single"
+										selected={date}
+										onSelect={(d) => {
+											setDate(d);
+											setMobileCalendarOpen(false);
+										}}
+									/>
+									<Button
+										onClick={() => setMobileCalendarOpen(false)}
+										className="mt-2 w-full"
+									>
+										Close
+									</Button>
+								</div>
+							</div>
+						)}
+					</>
+					<div className="sm:hidden">
+						{loading
+							? Array.from({ length: 5 }).map((_, i) => (
+									<SkeletonCard key={i} />
+								))
+							: Object.entries(groupedEvents).map(([date, events]) => (
+									<div key={date} className="mb-6 flex flex-col items-left">
+										<time className="text-2xl font-bold mb-2">{date}</time>
+										{events.map((event) => (
+											<EventCard event={event} key={event._id} />
+										))}
+									</div>
+								))}
+					</div>
+					<div className="hidden sm:block">
+						{loading
+							? Array.from({ length: 5 }).map((_, i) => (
+									<SkeletonCard key={i} />
+								))
+							: Object.entries(groupedEvents).map(([date, groupedEvents]) => (
+									<li key={date} className="mb-10 ms-4">
+										<div className="absolute w-3 h-3 bg-gray-800 rounded-full mt-1.5 -start-1.5 border border-white" />
+										<time className="mb-1 text-xl font-semibold leading-none">
+											{date}
+										</time>
+										{groupedEvents.map((event) => (
+											<EventCard event={event} key={event._id} />
+										))}
+									</li>
+								))}
+					</div>
 				</ol>
 
-				<div className="h-auto">
-					<div className="flex w-full items-center space-x-2 mb-4">
+				<div className="h-auto max-sm:mt-4">
+					<div className="flex w-full items-center space-x-2 mb-4 max-sm:flex-col max-sm:hidden">
 						<Button
 							variant={date && date >= today ? "default" : "secondary"}
 							onClick={() => setDate(new Date())}
-							className="rounded-[0.75rem] w-[70%] h-[3.125rem] font-medium"
+							className="rounded-[0.75rem] w-[70%] h-[3.125rem] font-medium max-sm:w-full"
 						>
 							Upcoming
 						</Button>
@@ -246,7 +318,7 @@ export default function Home() {
 								pastDate.setMonth(pastDate.getMonth() - 1);
 								setDate(pastDate);
 							}}
-							className="rounded-[0.75rem] w-[30%] h-[3.125rem] font-medium"
+							className="rounded-[0.75rem] w-[30%] h-[3.125rem] font-medium max-sm:w-full"
 						>
 							Past
 						</Button>
@@ -256,55 +328,10 @@ export default function Home() {
 						mode="single"
 						selected={date}
 						onSelect={setDate}
-						className="rounded-md border border-black border-opacity-25 bg-white"
+						className="rounded-md border border-black border-opacity-25 bg-white max-sm:hidden"
 					/>
 				</div>
 			</div>
 		</div>
 	);
 }
-
-const EventCard = ({ event }: { event: Event }) => {
-	return (
-		<Link href={`/events/${event.backlink}`} key={event._id}>
-			<div className="border flex border-[#D3D0D0] bg-white mt-6 rounded-2xl pl-[1.37rem] pr-[0.88rem] py-[1.41rem] w-full">
-				<div className="flex-1 flex flex-col gap-[0.5rem]">
-					<div className="flex items-center opacity-70 font-jakarta text-[#323232] text-[1rem] font-medium gap-[1.25rem]">
-						<p>
-							{new Date(event.date_from).toLocaleTimeString("en-US", {
-								hour: "numeric",
-								minute: "numeric",
-							})}
-						</p>
-						<div className="flex flex-row items-center gap-[0.12rem] w-[70%]">
-							<MapPin className="flex-shrink-0" size={20} />
-							<p className="line-clamp-1">{event.location}</p>
-						</div>
-					</div>
-					<h3 className="text-lg font-jakarta font-semibold text-gray-900 dark:text-white line-clamp-1">
-						{event.title}
-					</h3>
-					<p className="text-base font-semibold text-black dark:text-gray-400">
-						By{" "}
-						<span className="font-semibold text-[#1F76F9]">
-							{String(event?.organization?.name) || "Unknown Host"}
-						</span>
-					</p>
-					<p className="pr-10 text-base font-normal text-gray-500 dark:text-gray-400 line-clamp-2">
-						{event.description}
-					</p>
-				</div>
-				<div className="relative">
-					<img
-						src={event.photo_url || "/linktank_logo.png"}
-						alt={`${event.title} image`}
-						className="rounded-lg h-[9.5625rem] w-auto aspect-square object-cover"
-					/>
-					<div className="absolute top-2 right-2">
-						<SaveButton eventId={event._id} isSaved={true} />
-					</div>
-				</div>
-			</div>
-		</Link>
-	);
-};
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,74 +1,68 @@
 import Image from "next/image";
 import Link from "next/link";
 import {
-    SignInButton,
-    SignUpButton,
-    SignedIn,
-    SignedOut,
-    UserButton,
+	SignInButton,
+	SignUpButton,
+	SignedIn,
+	SignedOut,
+	UserButton,
 } from "@clerk/nextjs";
 import { currentUser } from "@clerk/nextjs/server";
 
 const Navbar = async () => {
-    const user = await currentUser();
+	const user = await currentUser();
 
-    return (
-        <nav className="flex items-center justify-between h-[6rem] p-4 bg-secondaryBlue border-b border-[#323232] border-opacity-15">
-            <div className="flex items-center mr-10">
-                <Link href="/" className="flex items-center">
-                    <Image
-                        src="/logo.png"
-                        alt="LinkTank"
-                        width={100}
-                        height={100}
-                        className="w-10 h-10 mr-[0.625rem]"
-                    />
+	return (
+		<nav className="flex items-center justify-between h-[6rem] p-4 bg-secondaryBlue border-b border-[#323232] border-opacity-15">
+			<div className="flex items-center mr-10">
+				<Link href="/" className="flex items-center">
+					<Image
+						src="/logo.png"
+						alt="LinkTank"
+						width={100}
+						height={100}
+						className="w-10 h-10 mr-[0.625rem]"
+					/>
 
-                    <span className="text-2xl font-bold">LinkTank</span>
-                </Link>
-            </div>
-            <div className="hidden md:flex items-center justify-between w-full">
-                <ul className="flex space-x-8 ml-6">
-                    <li>
-                        <Link
-                            href="/events"
-                            className="text-gray-600 hover:underline"
-                        >
-                            Events
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            href="#"
-                            className="text-gray-600 hover:underline"
-                        >
-                            My calendars
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            href="#"
-                            className="text-gray-600 hover:underline"
-                        >
-                            Organizations
-                        </Link>
-                    </li>
-                </ul>
-                <div className="flex items-center gap-x-4">
-                    <SignedOut>
-                        <SignInButton />
-                        <SignUpButton />
-                    </SignedOut>
-                    <SignedIn>
-                        <div className="flex items-center gap-x-4 bg-secondaryBlue hover:bg-secondaryBlue text-black shadow-none">
-                            <UserButton />
-                            <h4>{user?.fullName}</h4>
-                            <p>{user?.username}</p>
-                        </div>
-                    </SignedIn>
-                </div>
-            </div>
-        </nav>
-    );
+					<span className="text-2xl font-bold">LinkTank</span>
+				</Link>
+			</div>
+			<div className="hidden md:flex items-center justify-between w-full">
+				<ul className="flex space-x-8 ml-6">
+					<li>
+						<Link href="/events" className="text-gray-600 hover:underline">
+							Events
+						</Link>
+					</li>
+					<li>
+						<Link href="/mycalendars" className="text-gray-600 hover:underline">
+							My calendars
+						</Link>
+					</li>
+					<li>
+						<Link
+							href="/organizations"
+							className="text-gray-600 hover:underline"
+						>
+							Organizations
+						</Link>
+					</li>
+				</ul>
+				<div className="flex items-center gap-x-4">
+					<SignedOut>
+						<SignInButton />
+						<SignUpButton />
+					</SignedOut>
+					<SignedIn>
+						<div className="flex items-center gap-x-4 bg-secondaryBlue hover:bg-secondaryBlue text-black shadow-none">
+							<UserButton />
+							<h4>{user?.fullName}</h4>
+							<p>{user?.username}</p>
+						</div>
+					</SignedIn>
+				</div>
+			</div>
+		</nav>
+	);
 };
 export default Navbar;

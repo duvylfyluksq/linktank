5a0b928f30d8e98de0471ff1091d251b0e741ac0
Vedanta Neogi <vedantaneogi@gmail.com>
2025-04-01 22:50:51 +0530

    2 step sign in

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -56,7 +56,7 @@ export default function SignInPage() {
               submit
               className="w-full rounded-md bg-[#1C2329] px-3 py-3 text-center text-sm font-medium text-white shadow outline-none ring-1 ring-inset ring-zinc-950 hover:bg-zinc-800 focus-visible:outline-[1.5px] focus-visible:outline-offset-2 focus-visible:outline-zinc-950 active:text-white/70"
             >
-              Sign In
+              Continue
             </SignIn.Action>
             <div className="flex gap-2 w-full">
               <Clerk.Connection name="google" className="flex-1">
@@ -72,6 +72,46 @@ export default function SignInPage() {
                 </div>
               </Clerk.Connection>
             </div>
+          </SignIn.Step>
+          <SignIn.Step
+            name="verifications"
+            className="w-full space-y-6 rounded-2xl bg-white px-4 py-10 shadow-md ring-1 ring-black/5 sm:px-8"
+          >
+            <div className="flex items-center justify-center">
+              <Image
+                  src="/linktank_logo.png"
+                  alt="Linktank"
+                  width={100}
+                  height={100}
+                  className="w-10 h-10 mr-[0.625rem] rounded-full"
+              />
+              <span className="text-2xl font-bold">Linktank</span>
+            </div>
+            <Clerk.GlobalError className="block text-sm text-red-400" />
+            <div className="flex-row flex-grow items-center space-y-4 justify-center">
+              <div>
+                <Clerk.Field name="password">
+                  <Clerk.Label className="text-sm  font-medium text-zinc-950">Password</Clerk.Label>
+                  <Clerk.Input
+                    type="password"
+                    className="w-full rounded-md bg-white px-3.5 py-2 text-sm outline-none ring-1 ring-inset ring-zinc-300 hover:ring-zinc-400 focus:ring-[1.5px] focus:ring-zinc-950 data-[invalid]:ring-red-400"
+                  />
+                  <Clerk.FieldError className="block text-sm text-red-400" />
+                </Clerk.Field>
+                <SignIn.Action 
+                  navigate="forgot-password"
+                  className="underline underline-offset-4"
+                >
+                  Forgot password?
+                </SignIn.Action>
+              </div>
+            </div>
+            <SignIn.Action
+              submit
+              className="w-full rounded-md bg-[#1C2329] px-3 py-3 text-center text-sm font-medium text-white shadow outline-none ring-1 ring-inset ring-zinc-950 hover:bg-zinc-800 focus-visible:outline-[1.5px] focus-visible:outline-offset-2 focus-visible:outline-zinc-950 active:text-white/70"
+            >
+              Sign In
+            </SignIn.Action>
           </SignIn.Step>
         </SignIn.Root>
       </div>

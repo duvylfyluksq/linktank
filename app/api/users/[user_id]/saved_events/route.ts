03a5d6b150164d9d5c9e794a611f3d958989ab54
Vedanta Neogi <vedantaneogi@gmail.com>
2025-04-02 02:39:05 +0530

    Event schema error

--- app/api/users/[user_id]/saved_events/route.ts
+++ app/api/users/[user_id]/saved_events/route.ts
@@ -1,13 +1,14 @@
 import { NextResponse } from "next/server";
 import dbConnect from "@/lib/dbConnect";
 import User from "@/models/User";
+import Event from "@/models/Event"
 
 export async function GET(request: Request, { params }: { params: Promise<{ user_id: string}> }) {
   await dbConnect();
   try{
     const { user_id } = await params;
     const user = await User.findOne({ clerk_id: user_id })
-                        .populate("saved_events", "title date_from date_to location photo_url brief_description backlink is_date_range")
+                        .populate({path: "saved_events", model: Event})
                         .lean() as any; 
     if (!user) {
       return NextResponse.json({ success: false, message: "User not found" }, { status: 404 });

6bb77228c495e0a22724a5d9b22be0fb9ba778e3
Laxya Pahuja <laxya.pahuja8@gmail.com>
2025-04-12 20:34:03 +0530

    Update event-section.tsx

--- app/pages/event-section.tsx
+++ app/pages/event-section.tsx
@@ -2,8 +2,8 @@
 
 import { Calendar } from "lucide-react";
 import { useEffect, useState } from "react";
-import { Timeline } from "@/app/(event-listings)/timeline";
-import { EventCard } from "@/app/(event-listings)/EventCard";
+import { Timeline } from "@/components/timeline";
+import { EventCard } from "@/components/event-card";
 
 export default function PastEvents({ events }: { events: EventModel[] }) {
     const [isMobile, setIsMobile] = useState(false);
